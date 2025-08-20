177151d52bf55c6959a53cfd94958ea62b207437
Aditya Sahu <500096455@stu.upes.ac.in>
2025-08-20 20:30:50 +0530

    modified files

--- Arrays/Easy Problem/Max_Subarray_Sum.cpp
+++ Arrays/Easy Problem/Max_Subarray_Sum.cpp
@@ -30,7 +30,7 @@ int Kadane(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int arr[] = {3, -4, 5, 4, -1, 7, -8};
     int n = sizeof(arr) / sizeof(arr[0]);
     
     cout << "Max Subarray Sum using Brute Force: " << Brute(arr, n) << endl;
