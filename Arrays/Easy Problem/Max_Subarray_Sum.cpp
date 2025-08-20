22bcab7555bd625641bfc21d5cc7b837c7013e31
Aditya Sahu <500096455@stu.upes.ac.in>
2025-08-20 17:36:05 +0530

    New Commit

---
+++ Arrays/Easy Problem/Max_Subarray_Sum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Function to find the Maximus Sub Array Sum using Brute Force approach
+int Brute(int arr[], int n) {
+    int maxSum = arr[0];
+    for (int i = 0; i < n; i++) {
+        int currentSum = 0;
+        for (int j = i; j < n; j++) {
+            currentSum += arr[j];
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+    }
+    return maxSum;
+}
+ 
+// Function to find the Maximus Sub Array Sum using Kadane's Algorithm
+int Kadane(int arr[], int n) {
+    int maxSum = arr[0];
+    int currentSum = arr[0];
+    
+    for (int i = 1; i < n; i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+    
+    return maxSum;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Max Subarray Sum using Brute Force: " << Brute(arr, n) << endl;
+    cout << "Max Subarray Sum using Kadane's Algorithm: " << Kadane(arr, n) << endl;
+    
+    return 0;
+}
