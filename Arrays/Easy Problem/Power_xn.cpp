45d04960e2f14ea1ec94d86e01214d01da534c23
Aditya Sahu <500096455@stu.upes.ac.in>
2025-08-31 20:12:01 +0530

    new code

---
+++ Arrays/Easy Problem/Power_xn.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    double x = 2.1;
+    int n = 3;
+    cout << "Result: " << sol.myPow(x, n) << endl;
+    return 0;
+}
