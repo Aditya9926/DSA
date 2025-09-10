a9d025931ad2b87318694f8b52aca2cd87f40771
Aditya Sahu <500096455@stu.upes.ac.in>
2025-09-10 22:26:20 +0530

    modified code

--- Arrays/Medium Problem/Container_Most_Water.cpp
+++ Arrays/Medium Problem/Container_Most_Water.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// Using Brute force method 
+// Using Brute force method
 int maxwater(vector<int> &arr)
 {
     int n = arr.size();
@@ -11,9 +11,9 @@ int maxwater(vector<int> &arr)
     {
         for (int j = i + 1; j < n; j++)
         {
-            int w = j-i;
+            int w = j - i;
             int ht = min(arr[i], arr[j]);
-            int amount = w*ht;
+            int amount = w * ht;
             res = max(amount, res);
         }
     }
@@ -21,25 +21,27 @@ int maxwater(vector<int> &arr)
 }
 
 // Optimal Solution (2 pointer approach)
-int maxWater(vector<int> &arr) {
-  	int left = 0, right = arr.size() - 1;
+int maxWater(vector<int> &arr)
+{
+    int left = 0, right = arr.size() - 1;
     int res = 0;
-    while(left < right) {
+    while (left < right)
+    {
         int water = min(arr[left], arr[right]) * (right - left);
         res = max(res, water);
-      
-        if(arr[left] < arr[right])
+
+        if (arr[left] < arr[right])
             left += 1;
         else
             right -= 1;
     }
-  
+
     return res;
 }
 
 int main()
 {
     vector<int> arr = {2, 1, 8, 6, 4, 6, 5, 5};
-    cout << "Water stored in container using brute force method : "<< maxwater(arr);
-    cout << "\nWater stored in container using Optimal method : "<< maxWater(arr);
+    cout << "Water stored in container using brute force method : " << maxwater(arr);
+    cout << "\nWater stored in container using Optimal method : " << maxWater(arr);
 }
---
+++ Arrays/Medium Problem/Product_of_array.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// using brute force method
+vector<int> productarry(vector<int> &arr){
+    int n = arr.size();
+    vector<int> ans(n,1);
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0 ; j < n; j++){
+            if (i != j){
+                ans[i] *= arr[j]; 
+            }
+        }
+    }
+    return ans;
+}
+
+// optimal solution 
+vector<int> productarryop(vector<int> &arr){
+    int n = arr.size();
+    vector<int> sol(n,1);
+    vector<int> pre(n,1);
+    vector<int> sur(n,1);
+    for(int i = 1; i < n;i++){
+        pre[i] = pre[i-1] * arr[i-1];
+    }
+    for(int i = n-2; i>=0;i--){
+        sur[i] = sur[i+1] * arr[i+1];
+    }
+    for(int i = 0; i < n;i++){
+        sol[i] = pre[i]*sur[i];
+    }
+    return sol;
+}
+
+// Using Space Complexity
+vector<int> productarrysc(vector<int> &arr){
+    int n = arr.size();
+    int sur = 1;
+    vector<int> sna(n,1);
+    for(int i = 1; i < n;i++){
+        sna[i] = sna[i-1] * arr[i-1];
+    }
+    for(int i = n-2; i>=0;i--){
+        sur *= arr[i+1];
+        sna[i] *= sur;
+    }
+    return sna;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    vector<int> ans = productarry(arr);
+    vector<int> sol = productarryop(arr);
+    vector<int> sna = productarrysc(arr);
+    for(int val : ans){
+        cout << val << " ";
+    }
+    cout << "\n";
+    for(int val : sol){
+        cout << val << " ";
+    }
+    cout << "\n";
+    for(int val : sna){
+        cout << val << " ";
+    }
+    return 0;
+}
