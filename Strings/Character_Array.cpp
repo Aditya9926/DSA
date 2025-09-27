a2d66286c8462754036387acdb9b7bb1e32171bf
Aditya Sahu <500096455@stu.upes.ac.in>
2025-09-27 18:24:42 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ Strings/Character_Array.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int main(){
+    char str[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    char str1[] = "Hello";
+    int arr[] = {1, 2, 3, 4, 5};
+
+    cout << arr << endl;
+    cout << str << endl;
+    cout << str1 << endl;
+
+    char str2[50];
+    cout << "Enter your full name: ";
+    cin.getline(str2, 50); // Reads the entire line including spaces
+    cout << "Your full name is: " << str2 << endl;
+
+    int len = 0;
+    for(int i = 0; str2[i] != '\0'; i++){
+        len++;
+    }
+    cout << "Length of your name is: " << len << endl;
+
+    cout << "Length using strlen function: " << strlen(str2) << endl;
+
+    for(auto i : str2){
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    cout << "Enter your full name: ";
+    cin.getline(str2, 50,'h'); // Reads the entire line including spaces
+    cout << "Your full name is: " << str2 << endl;
+
+    getchar(); // To consume the newline character left in the buffer
+    cout << "Enter your name: ";
+    cin >> str2; // Only reads input until the first whitespace
+    cout << "Your name is: " << str2 << endl;
+
+    return 0;
+}
