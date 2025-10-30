bdc3310f4dc297c0377d0b730e216cd781c6b1d7
Aditya Sahu <500096455@stu.upes.ac.in>
2025-10-30 16:21:35 +0530

    initial commit

---
+++ Arrays/Easy Problem/2Sum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+// 2Sum using Hashing.
+vector<int> twosum(vector<int> &arr, int tar){
+    unordered_map<int,int> m;
+    vector<int> ans;
+    for(int i = 0; i < arr.size(); i++){
+        int f = arr[i];
+        int s = tar - f;
+        if(m.find(s) != m.end()){
+            ans.push_back(i);
+            ans.push_back(m[s]);
+            break;
+        }
+        m[f] = i;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {2,7,11,15};
+    int tar = 9;
+    cout << "The index is: ";
+    vector<int> res = twosum(arr, tar); 
+    for(int i : res){
+        cout << i << " ";
+    }
+    return 0;
+}
