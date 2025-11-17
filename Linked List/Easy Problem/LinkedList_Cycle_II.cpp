94a41e535b78b2ca1dd6eff8bfd67d990b333b46
Aditya Sahu <500096455@stu.upes.ac.in>
2025-11-17 13:49:43 +0530

    initial commit

---
+++ Linked List/Easy Problem/LinkedList_Cycle_II.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class ListNode{
+    public :
+        int val;
+        ListNode* next;
+        ListNode(int x){
+            val = x;
+            next = NULL;
+        }
+};
+
+class Solution {
+    public:
+        ListNode *detectCycle(ListNode *head) {
+            ListNode* slow = head;
+            ListNode* fast = head;
+            bool iscycle = false;
+            while(fast != NULL && fast->next != NULL){
+                slow = slow->next;
+                fast = fast->next->next;
+                if(slow == fast){
+                    iscycle = true;
+                    break;
+                }
+            }
+            if(!iscycle){
+                return NULL;
+            }
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+};
+
+int main(){
+    ListNode* Node = new ListNode(3);
+    Node->next = new ListNode(2);
+    Node->next->next = new ListNode(0);
+    Node->next->next->next = new ListNode(-4);
+    Node->next->next->next->next = Node->next; // Creating a cycle for testing
+
+    ListNode* head = Node;
+    Solution solution;
+    ListNode* cycleStartNode = solution.detectCycle(head);
+    if(cycleStartNode){
+        cout << "Cycle detected at node with value: " << cycleStartNode->val << endl;
+    } else {
+        cout << "No cycle detected in the linked list." << endl;
+    }
+}
