6a85fe50b35f9139af0a8f2c313849c9089b027a
Aditya Sahu <500096455@stu.upes.ac.in>
2025-11-22 10:44:04 +0530

    updated file

--- Linked List/Circular_Linked_List.cpp
+++ Linked List/Circular_Linked_List.cpp
@@ -21,6 +21,7 @@ class CircularList{
         head = tail = NULL;
     }
 
+    // Function to insert a node at the beginning of the Circular Linked List.
     void insertbeg(int val){
         Node* newnode = new Node(val);
         if(tail == NULL){
@@ -31,6 +32,58 @@ class CircularList{
             newnode->next = head;
             head = newnode;
             tail->next = head;
+        }
+    }
+
+    // Function to insert a node at the end of the Circular Linked List.
+    void insertend(int val){
+        Node* newnode = new Node(val);
+        if(tail == NULL){
+            head = tail = newnode;
+            tail->next = head;
+        }
+        else{
+            tail->next = newnode;
+            tail = newnode;
+            tail->next = head;
+        }
+    }
+
+    // Delete a node from the beginning of the Circular Linked List.
+    void deletebeg(){
+        if(head == NULL){
+            return;
+        }
+        else if(head == tail){
+            delete head;
+            head = tail = NULL;
+        }
+        else{
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    // Delete a node from the end of the Circular Linked List.
+    void deleteend(){
+        if(head == NULL){
+            return;
+        }
+        else if(head == tail){
+            delete head;
+            head = tail = NULL;
+        }
+        else{
+            Node* temp = head;
+            while(temp->next != tail){
+                temp = temp->next;
+            }
+            temp->next = head;
+            delete tail;
+            tail = temp;
         }
     }
 
@@ -54,5 +107,13 @@ int main(){
     cll.insertbeg(1);
     cll.insertbeg(2);
     cll.insertbeg(3);
+    cll.insertend(4);
+    cll.insertend(5);
+    cout << "Circular Linked List before deleting element : ";
+    cll.printLL();
+    
+    cll.deletebeg();
+    cll.deleteend();
+    cout << "Circular Linked List after deleting element : ";
     cll.printLL();
 }
